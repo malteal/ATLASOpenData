3795329c0f2a1c44f871c45ce04a429468d4425f
Andreas Hermansen <andreas.hermansen@unige.ch>
2025-02-21 18:22:25 +0100

    Added datamodule.

---
+++ src/torch_data/constants.py
@@ -0,0 +1,50 @@
+# TODO: @ANDREAS Move below to concrete datafiles.
+KEYS = ['csts', 'csts_columns', 'csts_culens', 'event_info', 'event_info_columns', 'jets', 'jets_columns']
+CSTS_COLUMNS = [
+    "b'InDetTrackParticlesAuxDyn.phi'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelHoles'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelSharedHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelDeadSensors'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTHoles'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTSharedHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTDeadSensors'",
+    "b'InDetTrackParticlesAuxDyn.qOverP'",
+    "b'InDetTrackParticlesAuxDyn.numberOfInnermostPixelLayerHits'",
+    "b'InDetTrackParticlesAuxDyn.chiSquared'",
+    "b'InDetTrackParticlesAuxDyn.numberDoF'",
+    "b'InDetTrackParticlesAuxDyn.d0'",
+    "b'InDetTrackParticlesAuxDyn.z0'",
+    "b'InDetTrackParticlesAuxDyn.theta'",
+    "b'InDetTrackParticlesAuxDyn.vz'",
+    "b'InDetTrackParticlesAuxDyn.numberOfTRTHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfTRTOutliers'",
+    "b'InDetTrackParticlesAuxDyn.eta'",
+    "b'InDetTrackParticlesAuxDyn.pt'"
+]
+CSTS_COLUMNS_LOOKUP = {s: idx for idx, s in enumerate(CSTS_COLUMNS)}
+JETS_COLUMNS = [
+    "b'InDetTrackParticlesAuxDyn.phi'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelHoles'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelSharedHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfPixelDeadSensors'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTHoles'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTSharedHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfSCTDeadSensors'",
+    "b'InDetTrackParticlesAuxDyn.qOverP'",
+    "b'InDetTrackParticlesAuxDyn.numberOfInnermostPixelLayerHits'",
+    "b'InDetTrackParticlesAuxDyn.chiSquared'",
+    "b'InDetTrackParticlesAuxDyn.numberDoF'",
+    "b'InDetTrackParticlesAuxDyn.d0'",
+    "b'InDetTrackParticlesAuxDyn.z0'",
+    "b'InDetTrackParticlesAuxDyn.theta'",
+    "b'InDetTrackParticlesAuxDyn.vz'",
+    "b'InDetTrackParticlesAuxDyn.numberOfTRTHits'",
+    "b'InDetTrackParticlesAuxDyn.numberOfTRTOutliers'",
+    #"b'InDetTrackParticlesAuxDyn.eta'",
+    #"b'InDetTrackParticlesAuxDyn.pt'"
+]
+JETS_COLUMNS_LOOKUP = {s: idx for idx, s in enumerate(JETS_COLUMNS)}
---
+++ src/torch_data/utils.py
@@ -0,0 +1,52 @@
+"""Module containing utility classes for the datamodule."""
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TypedDict
+
+from torch import Tensor
+
+from constants import JETS_COLUMNS
+from src.torch_data.constants import CSTS_COLUMNS
+
+
+class StreamData(TypedDict):
+    """Dictionary containing data for a batch of jets."""
+    jets: Tensor
+    csts: Tensor
+    split: Tensor
+
+
+@dataclass
+class StreamDataConfig:
+    """Configuration for the stream dataset."""
+    batch_size: int = 1000
+    jet_features: list[str] | None = None
+    csts_features: list[str] | None = None
+    num_jets: int | None = None
+    num_csts: int | None = None
+
+    def __post_init__(self):
+        if self.jet_features is None:
+            self.jet_features = JETS_COLUMNS
+        if self.csts_features is None:
+            self.csts_features = CSTS_COLUMNS
+
+
+@dataclass
+class DataPaths:
+    """Collection of paths to the datamodule."""
+    train_path: Path | str
+    val_path: Path | str
+    test_path: Path | str | None = None
+    predict_path: Path | str | None = None
+
+    def __post_init__(self):
+        if self.predict_path is None:
+            self.predict_path = self.val_path
+
+
+@dataclass
+class DataLoaderConfig:
+    """DataLoader configuration."""
+    num_workers: int
+    pin_memory: bool = True
